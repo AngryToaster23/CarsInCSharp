4d048e18cb027d6e7531a687cd18fcf9f0136053
AngryToaster23 <156143445+AngryToaster23@users.noreply.github.com>
2024-01-16 20:23:09 -0500

    first version
    making cars and stuff

---
+++ carsInCsharp/carsVroom/vroomCat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carsVroom
+{
+    public class vroomCat
+    {
+        //define attributes of car
+        public string Make { get; set ; }
+        public string Model { get; set ; }
+        public int Year { get; set ; }
+        public string Color { get; set ; }
+        public int Mileage { get; set ; }
+        public string VIN { get; set ; }
+
+        //constructor
+
+        public vroomCat(string make, string model, int year, string color, int mileage, string VIN)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+            Mileage = 0;
+        }
+        //methods
+        public void Drive(int miles)
+        {
+            if (miles > 0)
+            {
+                Mileage += miles;
+            }
+        }
+        public void Displayinfo()
+        {
+            Console.WriteLine($"Make: {Make}, Model {Model}, Year: {Year}, Color: {Color}, Mileage: {Mileage}"); 
+        }
+        public int getMileage()
+        {
+            return Mileage;
+        }
+
+        
+
+    }
+}
---
+++ carsInCsharp/classProject/Program.cs
@@ -0,0 +1,57 @@
+﻿//show main menue
+
+using carsVroom;
+//variables
+int intUserInput = 0;
+string strUserInput = "";
+List<vroomCat> cars = new List<vroomCat>();
+
+while (intUserInput != 4)
+{
+    Console.WriteLine("Welcome to THE DeAlErShIp\n");
+    Console.WriteLine("--------------------");
+    Console.WriteLine("1. Add a new car\n2. List all cars\n3. Remove a Car\n4. Remove Car");
+
+    try
+    {
+        strUserInput = Console.ReadLine();
+        intUserInput = int.Parse(strUserInput);
+    }    catch (Exception ex)
+    {
+        switch (intUserInput)
+        {
+            case 0:
+                break;
+            case 1:
+                Console.WriteLine("Add a new car");
+                string make, model, color;
+                int year;
+                Console.WriteLine("Enter the make");
+                make=Console.ReadLine();
+                Console.WriteLine("Enter the model");
+                model = Console.ReadLine();
+                Console.WriteLine("Enter the color");
+                color = Console.ReadLine();
+                Console.WriteLine("Enter the year");
+                year = int.Parse(Console.ReadLine());
+                vroomCat newCar = new vroomCat(make, model, year, color);
+                cars.Add(newCar);
+                break;
+            case 2:
+                Console.WriteLine("List all cars");
+                foreach (var vroomCat in cars)
+                {
+
+                }
+                break;
+            case 3:
+                Console.WriteLine("Remove Car");
+                break;
+            case 4:
+                Console.WriteLine("Exit");
+                break;
+        }
+    }
+
+}
+
